ec086ce1cbf8ad187bd5779440efcc063f145190
alexandrustefanescu <stefanescualexandru1994@gmail.com>
2022-09-08 20:23:45 +0200

    feature (client): add styling and component
    Added styling for container and Postbox component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className=''>
+    <div className='max-w-5xl my-7 mx-auto'>
       <Head>
         <title>Reddit Clone</title>
         <link
@@ -13,7 +14,9 @@ const Home: NextPage = () => {
       </Head>
 
       {/* Postbox */}
-      <div>
+      <PostBox />
+
+      <div className='flex'>
         {/* Feed */}
       </div>
     </div>
